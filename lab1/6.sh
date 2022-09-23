c3662299ae2ace4e7072e896c2f54d909b2963c3
VMihail <83347281+VMihail@users.noreply.github.com>
2022-09-23 19:54:36 +0300

    Update 6.sh

--- lab1/6.sh
+++ lab1/6.sh
@@ -1,4 +1,4 @@
-#bin/bash
+#!bin/bash
 awk '$3 == "(II)"' /var/log/anaconda/X.log | sed 's/(II)/Information:/' > full.log
 awk '$3 == "(WW)"' /var/log/anaconda/X.log | sed 's/(WW)/Warning:/' >> full.log
 cat full.log
